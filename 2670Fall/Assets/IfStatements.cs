bfaff95ae54ae91a02b70c93bb8b06579a72377b
Meaghan Searle <msearle88@gmail.com>
2018-08-20 14:23:17 -0600

    Class day one

---
+++ 2670Fall/Assets/IfStatements.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+
+	public int Score;
+	public float Speed;
+	public bool LightsOn;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (true)
+		{
+			//do work
+		}
+
+		if (Score == 10)
+		{
+			print("Ten");
+		}
+
+		if (Speed >= 4)
+		{
+			print("Gotta go fast!");
+		}
+
+		if (!LightsOn && Speed > 4)
+		{
+			print("Lights out.");
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
