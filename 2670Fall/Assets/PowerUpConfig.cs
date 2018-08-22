642345b906187aa86c9496a299bf6a83a9108d66
Meaghan Searle <msearle88@gmail.com>
2018-08-22 14:28:31 -0600

    In class shape buttons

---
+++ 2670Fall/Assets/PowerUpConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PowerUpConfig")]
+public class PowerUpConfig : ScriptableObject
+{
+
+	public GameObject Prefab;
+	public Sprite SpriteImage;
+	public Color SpriteColor;
+
+	public void OnConfigure()
+	{
+		var newPrefab = Instantiate(Prefab);
+		newPrefab.name = this.name;
+
+		var newSprite = newPrefab.GetComponent<SpriteRenderer>();
+		newSprite.sprite = SpriteImage;
+		newSprite.color = SpriteColor;
+	}
+}
