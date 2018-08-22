0bf0ef9f20757dbd6511fe5d6d7eb9a2d6680fb0
Meaghan Searle <msearle88@gmail.com>
2018-08-21 17:36:31 -0700

    death

---
+++ 2670Fall/Assets/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+
+	public FloatValue FloatValue; 
+
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
