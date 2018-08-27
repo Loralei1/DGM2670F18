b84d4d644d937bace1c60c6553080cf86fa77765
Meaghan Searle <msearle88@gmail.com>
2018-08-27 14:29:19 -0600

    Event introduction

---
+++ 2670Fall/Assets/EventScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventScript : MonoBehaviour
+{
+
+    public UnityEvent MyEvent;
+    public UnityEvent Event;
+
+    private void Start()
+    {
+        MyEvent.Invoke();
+    }
+
+    private void OnMouseDown()
+    
+        {
+          Event.Invoke();  
+        }
+   
+
+    
+
+    /*private void OnMouseDown()
+    {
+        Event.Invoke();
+    }*/
+}
