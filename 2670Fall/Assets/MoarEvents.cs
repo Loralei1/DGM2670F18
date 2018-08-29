cc89e3d79dcf810fca84b9a36f597854fabc491d
Meaghan Searle <msearle88@gmail.com>
2018-08-29 14:23:05 -0600

    events

---
+++ 2670Fall/Assets/MoarEvents.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MoarEvents : MonoBehaviour
+{
+
+	public UnityEvent NewEvent;
+
+	public UnityEvent OnEvent;
+	public UnityEvent ParticleEvent;
+	//Events are not dependent on other scripts
+	//Objects, functions, and variables can be referenced/called by an Event
+	//Function(){do stuff}; Unity event is a function with no body. 
+	//Class UnityEvent; Invoke(){Code visualized by unity};
+	private void OnMouseOver()
+	{
+		NewEvent.Invoke();                                                                       
+	}
+
+	private void OnMouseExit()
+	{
+		OnEvent.Invoke();
+	}
+
+	private void OnMouseDown()
+	{
+		ParticleEvent.Invoke();
+	}
+}
