dfa24427a2168ee19f986b82fc069a32d103375d
Meaghan Searle <msearle88@gmail.com>
2018-09-03 21:50:57 -0700

    messing around with events

---
+++ 2670Fall/Assets/EventFolder/BecomeInvisibleEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BecomeInvisibleEvent : MonoBehaviour
+{
+
+	public UnityEvent Event;
+
+	private void OnBecameInvisible()
+	{
+		Event.Invoke();
+	}
+}
