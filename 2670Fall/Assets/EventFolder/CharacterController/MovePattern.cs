cc80d3dbd59165148dc9ea601da28ad870c8a44f
Meaghan Searle <msearle88@gmail.com>
2018-09-10 14:23:43 -0600

    Rotate and jump

---
+++ 2670Fall/Assets/EventFolder/CharacterController/MovePattern.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class MovePattern : ScriptableObject
+{
+
+	//public float speed = 6.0F;
+	public FloatData jumpSpeed;
+	public FloatData gravity;
+
+	public FloatData MoveX, MoveY, MoveZ;
+	public FloatData RotX, RotY, RotZ;
+
+
+	private Vector3 moveDirection;
+	private Vector3 rotDirection;
+
+	public void Invoke(CharacterController controller, Transform transform)
+	{
+		if (controller.isGrounded)
+		{
+			moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
+			rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
+			transform.Rotate(rotDirection);
+			moveDirection = transform.TransformDirection(moveDirection);
+			//moveDirection *= speed;
+			if (Input.GetButton("Jump"))
+			{
+				moveDirection.y = jumpSpeed.Value;
+			}
+		}
+
+		moveDirection.y -= gravity.Value * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
+
