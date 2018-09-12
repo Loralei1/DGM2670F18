e643c8cb0b4dd48bfe86d9134cd1cdc74a76fed1
Meaghan Searle <msearle88@gmail.com>
2018-09-12 14:24:49 -0600

    Not grounded moved patterns

--- 2670Fall/Assets/EventFolder/CharacterController/MovePattern.cs
+++ 2670Fall/Assets/EventFolder/CharacterController/MovePattern.cs
@@ -14,26 +14,40 @@ public class MovePattern : ScriptableObject
 	public FloatData RotX, RotY, RotZ;
 
 
-	private Vector3 moveDirection;
+	protected Vector3 moveDirection;
 	private Vector3 rotDirection;
 
-	public void Invoke(CharacterController controller, Transform transform)
+	public virtual void Invoke(CharacterController controller, Transform transform)
 	{
 		if (controller.isGrounded)
 		{
-			moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
-			rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
-			transform.Rotate(rotDirection);
-			moveDirection = transform.TransformDirection(moveDirection);
+			
+			Move(transform);//moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
+			//rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
+			//transform.Rotate(rotDirection);
+			//moveDirection = transform.TransformDirection(moveDirection);
 			//moveDirection *= speed;
-			if (Input.GetButton("Jump"))
-			{
-				moveDirection.y = jumpSpeed.Value;
-			}
+			//if (Input.GetButton("Jump"))
+			//{
+				//moveDirection.y = jumpSpeed.Value;
+			//}
 		}
 
+		FinalMove(controller);
+	}
+
+	protected void FinalMove(CharacterController controller)
+	{
 		moveDirection.y -= gravity.Value * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
+	}
+
+	protected void Move(Transform transform)
+	{
+		moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
+		rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
+		transform.Rotate(rotDirection);
+		moveDirection = transform.TransformDirection(moveDirection);
 	}
 }
 
