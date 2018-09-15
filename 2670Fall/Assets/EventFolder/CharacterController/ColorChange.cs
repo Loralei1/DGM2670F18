e685b30f1c44f36479dda5f9465e5337b007c618
Meaghan Searle <msearle88@gmail.com>
2018-09-15 12:51:15 -0700

    switch and loops, still need to be config

---
+++ 2670Fall/Assets/EventFolder/CharacterController/ColorChange.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+
+		public enum gates {Gate, Endgate, RedGate};
+
+		public gates gatename; 
+
+		
+		void OnTriggerExit(Collider other) {
+
+			switch (gatename)
+
+			{case (gates.Gate):
+
+					//do color change stuff
+
+					other.GetComponent<Renderer> ().material.color = GetComponent<Renderer> ().material.color;
+
+					break;
+
+				case (gates.RedGate):
+
+					print ("Red.");
+
+					break;
+
+				case (gates.Endgate):
+
+					print ("Wh-w-wait a minute? Whatever happened to just plain old lavender blue dilly dilly dilly dilly? Silly.");
+
+					break; 
+
+			}
+
+			
+
+
+
+		
+
+		}
+
+	
+
+	}	
---
+++ 2670Fall/Assets/EventFolder/CharacterController/PowerUpGive.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpGive : MonoBehaviour
+{
+
+	public class Trigger : MonoBehaviour
+	{
+
+
+
+		public PowerUp PowerUpTransfer;
+
+		public GameObject other;
+
+		public float PowerLevel;
+
+
+
+		private void OnTriggerEnter(Collider obj)
+
+
+
+		{
+
+			obj.GetComponent<MoveExample>().MovePattern = PowerUpTransfer.Transfer();
+
+
+
+			if (other.gameObject.CompareTag("Player"))
+			{
+
+				gameObject.SetActive(false);
+
+			}
+
+		}
+
+	}
+}
+
+	
