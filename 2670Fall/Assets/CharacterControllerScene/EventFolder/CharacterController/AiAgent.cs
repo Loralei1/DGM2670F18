77923e4d25eb0b10b4b3303f9134425a9fb0028d
Meaghan Searle <msearle88@gmail.com>
2018-09-20 12:12:39 -0700

    Moved folders

---
+++ 2670Fall/Assets/CharacterControllerScene/EventFolder/CharacterController/AiAgent.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+using UnityEngine.AI;
+//using UnityEngine.Experimental.PlayerLoop;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AiAgent : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    public Transform Destination;
+    //public bool CanChase;
+    public Transform postpoint;
+    private Transform finalDestination;
+    public FloatData Speed;
+    
+    private void Start()
+
+    {
+        
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = Speed.Value;
+        finalDestination = transform;
+    }
+
+    private void OnTriggerEnter(Collider obj)
+    {
+       if(obj.transform == Destination)
+        finalDestination = Destination;
+        //CanChase = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        finalDestination = postpoint;
+        //CanChase = false;
+    }
+
+    private void Update()
+    {
+        
+        {
+            agent.destination = finalDestination.position;
+        }
+        
+       
+    }
+
+    
+        
+    
+}
+
