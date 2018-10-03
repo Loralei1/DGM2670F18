af4afc27dec40173e296c8236134c4bf7348864e
Meaghan Searle <msearle88@gmail.com>
2018-10-03 13:19:25 -0700

    Color change example

---
+++ 2670Fall/Assets/CharacterControllerScene/EventFolder/CharacterController/Hyper Casual Demo/MatchID.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MatchID : MonoBehaviour
+{
+
+	public NameID ID;
+	public UnityEvent OnMatch;
+
+	public void Invoke(NameID id)
+
+	{
+		if (id == ID)
+		{
+			OnMatch.Invoke();
+		}
+	}
+	
+}
+
+public class NameID
+{
+}
+
+
