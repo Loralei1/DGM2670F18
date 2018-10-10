2d713fe8d4e7f76762ce5c02f6bc443558a35419
Meaghan Searle <msearle88@gmail.com>
2018-10-10 14:29:17 -0600

    Class demo

---
+++ 2670Fall/Assets/CharacterControllerScene/EventFolder/CharacterController/Hyper Casual Demo/ChangeColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ChangeColor : ScriptableObject
+{
+	public ColorData Color; 
+
+
+	public void Call(SpriteRenderer spriteRenderer)
+	{
+		spriteRenderer.color = Color.Value; 
+	}
+	
+	public void Call(Material material)
+	{
+		material.color = Color.Value; 
+	}
+}
--- 2670Fall/Assets/CharacterControllerScene/EventFolder/CharacterController/Hyper Casual Demo/MatchID.cs
+++ 2670Fall/Assets/CharacterControllerScene/EventFolder/CharacterController/Hyper Casual Demo/MatchID.cs
@@ -1,14 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Security.Permissions;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Experimental.UIElements;
 
 public class MatchID : MonoBehaviour
 {
 
 	public NameID ID;
 	public UnityEvent OnMatch;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Type ObjectID;
+		//var id = other.GetComponent(ObjectID).ID;
+		Call(ID);
+	}
+
+	private void Call(NameID id)
+	{
+		throw new NotImplementedException();
+	}
 
 	public void Invoke(NameID id)
 
