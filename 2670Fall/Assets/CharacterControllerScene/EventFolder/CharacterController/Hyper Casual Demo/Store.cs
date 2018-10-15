c3bb690ff195fe121ede56e4bd88c6a0186ae0f7
Meaghan Searle <msearle88@gmail.com>
2018-10-15 14:30:14 -0600

    store demo

---
+++ 2670Fall/Assets/CharacterControllerScene/EventFolder/CharacterController/Hyper Casual Demo/Store.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Store", menuName = "Store/StoreFront")]
+public class Store : ScriptableObject
+{
+
+	public Objects Available;
+	public Objects Purchased; 
+	
+	public void MakePurchase(Object obj)
+	{
+		for (var index = 0; index < Available.ObjectList.Count; index++)
+		{
+			var availableObject = Available.ObjectList[index];
+			if (availableObject == obj)
+			{
+				Purchased.ObjectList.Add(obj);
+				Available.ObjectList.Remove(availableObject);
+			}
+		}
+	}
+
+}
