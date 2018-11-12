276857f33d22f2eedd046de0cb535bd01c23ee73
Meaghan Searle <msearle88@gmail.com>
2018-11-12 14:38:07 -0700

    In Class demo

---
+++ 2670Fall/Assets/CharacterControllerScene/EventFolder/CharacterController/Destroyer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+
+	public float HoldTime = 2;
+	public bool UseTrigger = true; 
+
+	IEnumerator Start()
+	{
+		yield return new WaitForSeconds(HoldTime);
+		Destroy(gameObject);
+	}
+
+	public void DestroyObj()
+	{
+		Destroy(gameObject);
+	}
+
+	public void DestroyAny(Object obj)
+	{
+		Destroy(obj);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (UseTrigger)
+		{
+			Destroy(other.gameObject);
+		}
+	}
+}
---
+++ 2670Fall/Assets/CharacterControllerScene/EventFolder/CharacterController/Hyper Casual Demo/MouseFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+	public Vector3 tempPos; 
+	
+	
+	private void Update()
+	{
+		//transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		tempPos = Input.mousePosition;
+		tempPos.z = 0;
+		transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition)*5;
+		print(Camera.main.ScreenToViewportPoint(tempPos));
+	}
+}
